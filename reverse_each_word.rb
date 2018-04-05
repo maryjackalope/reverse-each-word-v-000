bcc9dd921a2b1c8e7338c349e281d86ea080cea5
Mary Jack <maryjackinthebox@gmail.com>
2018-04-05 23:03:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,6 @@ def reverse_each_word(string)
   reversed= string.split.each {|element| element.reverse }.join(" ")
 end 
   
-#def 
+def reverse_each_word(str)
+ str.split.collect { |e| e.reverse }.join(" ")
+end
